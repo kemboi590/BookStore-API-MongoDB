7b4cd9e688d891fbf409464e0676389be9aafa88
kemboi590 <bkemboi590@gmail.com>
2024-09-26 19:42:29 +0300

    Add initial project files and configurations

---
+++ Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,9 @@
+
+namespace BookStoreApi.Models;
+
+public class BookStoreDatabaseSettings
+{
+    public string ConnectionStrings { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string BooksCollectionName { get; set; } = null!;
+}
